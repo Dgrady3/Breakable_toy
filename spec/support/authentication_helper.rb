1ddea6a9a7ee8abd7ecccdf825d67040622accf4
Dgrady3 <joegrady1@gmail.com>
2014-10-09 13:45:53 -0400

    fixed helper method

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -3,6 +3,6 @@ module AuthenticationHelper
     visit new_user_session_path
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
-    click_on "Sign in"
+    click_on "Log in"
   end
 end
