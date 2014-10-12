448119d94ecf577ff6c6b9de9740c319b0ef91a3
Dgrady3 <joegrady1@gmail.com>
2014-10-11 20:09:45 -0400

    add model tests passing

--- spec/models/categories_spec.rb
+++ spec/models/categories_spec.rb
@@ -5,9 +5,4 @@ describe Category do
   describe 'validations' do
     it { should validate_presence_of :name }
   end
-
-  describe 'associations' do
-    it { should has_many :categorizations }
-
-  end
 end
--- spec/models/categorizations_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-describe Categorization do
-
-
-  describe 'associations' do
-    it { should belong_to :category }
-  end
-end
