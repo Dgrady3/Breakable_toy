b3561704147428608d4eaf9f01e31af1b1625dd7
Dgrady3 <joegrady1@gmail.com>
2014-10-17 22:09:34 -0400

    updates

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,4 +1,7 @@
 class WelcomesController < ApplicationController
   def index
+    require 'net/http'
+    workouts = Net::HTTP.get(URI('https://www.kimonolabs.com/api/7unf33cw?apikey=IqRSjYM5lNhzd0FnxGH5VIRWFZzR789y'))
+    binding.pry
   end
 end
